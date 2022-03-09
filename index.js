0e36c76fc4da5e0ca0ce6dcc68ad48583f70a45f
unknown <kylelarsen12@gmail.com>
2022-03-09 18:38:34 -0500

    Started work on js

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-let weeklyExpenses = 0
-let monthlyExpenses = 0
-let annualExpenses = 0
+let weeklyExpenses = 0;
+let monthlyExpenses = 0;
+let annualExpenses = 0;
 
 // Your Code Here
+
+//Get user lunch expenses
+window.prompt(weeklyExpenseQuestions[0]);
+let lunchCost = window.prompt(weeklyExpenseQuestions[0]);
+console.log(lunchCost);
+
+//Conver from string to float
+let lunchNum = window.prompt(parseFloat(weeklyExpenseQuestions[0]));
+console.log(lunchNum);
